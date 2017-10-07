28db16bcd678281c870702403d31df4c5f79f203
speed <speed@MacBook-Pro-van-speed.local>
2017-10-07 21:18:48 +0200

    V2.6

--- server/tasks.js
+++ server/tasks.js
@@ -130,13 +130,4 @@ Meteor.methods({
 
 
 });
-Meteor.startup(function(){
-  // Tasks.remove({owner:"P4ex3hRxWuvFgPDj2"});
-  // for (var i = 0; i < 100; i++) {
-  //   if(i%2 == 0)
-  //   Tasks.insert({owner:"P4ex3hRxWuvFgPDj2",content:`new task ${i}`,checked:false});
-  //   else {
-  //     Tasks.insert({owner:"P4ex3hRxWuvFgPDj2",content:`new task ${i}`,checked:true});
-  //   }
-  // }
-});
+
